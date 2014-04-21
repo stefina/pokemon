1a93d191126a8fb39e9fb119c4af74363c813bc8
Stefanie Taepke <s.taepke@student.htw-berlin.de>
2014-04-21 20:01:46 +0200

    osm-reader
    added osm-reader and included look up for tag by location-rectangle

--- app/controllers/pokemonController.js
+++ app/controllers/pokemonController.js
@@ -1,10 +1,42 @@
 var mongoose = require('mongoose'),
 	Pokemon = mongoose.model('Pokemon'),
-	Trainer = mongoose.model('Trainer');
+	Trainer = mongoose.model('Trainer'),
+	osmRead = require('osm-read');
 
 exports.checkLocationForPokemon = function(req, res){
 	var node = req.query.location;
-	// console.log(node);
+	
+
+	var latitude = node.loc_latitude;
+	var longitude = node.loc_longitude;
+
+	// create a rectangle
+	var longitude_a, longitude_b, latitude_a, latitude_b;
+	longitude_a = round(longitude);
+	longitude_b = round(longitude) + 0.1;
+	latitude_a = round(latitude);
+	latitude_b = round(latitude) + 0.1;
+
+	osmRead.parse({
+		// url: 'http://overpass-api.de/api/interpreter?data=node(51.93315273540566%2C7.567176818847656%2C52.000418429293326%2C7.687854766845703)%5Bhighway%3Dtraffic_signals%5D%3Bout%3B',
+		url: 'http://overpass-api.de/api/interpreter?data=node%28' + latitude_a + '%2C' + longitude_a + '%2C' + latitude_b + '%2C' + longitude_b + '%29%5B%22natural%22%5D%3Bout%20body%3B%0A',
+		format: 'xml',
+		endDocument: function(){
+			console.log('document end');
+		},
+		bounds: function(bounds){
+			console.log('bounds: ' + JSON.stringify(bounds));
+		},
+		node: function(node){
+			console.log('node: ' + JSON.stringify(node));
+		},
+		way: function(way){
+			console.log('way: ' + JSON.stringify(way));
+		},
+		error: function(msg){
+			console.log('error: ' + msg);
+		}
+	});
 
 	Pokemon.checkArea(node, function(err, pokemon) {
 		res.json({ location: node, pokemon: pokemon });
@@ -67,6 +99,10 @@ exports.fight = function(req, res){
 			res.render('fight/pokemonFight', { trainerPokemon: trainerPokemon, enemyPokemon: pokemon });
 		});
 	});
+}
+
+function round(val){
+	return Math.round(val*100000)/100000;
 }
 
 // exports.moveToPC = function(req, res) {
